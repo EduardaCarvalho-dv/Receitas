00d97514ddd080c865468968fc43ce23bb606f89
EduardaCarvalho-dv <eduardasantos132018@gmail.com>
2024-12-18 12:24:36 -0300

    vb

--- src/componentes/paginacao/home/home.jsx
+++ src/componentes/paginacao/home/home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Header from "../header/Header";
+import Header from "../header/Header.jsx";
 import Card from "react-bootstrap/Card";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
